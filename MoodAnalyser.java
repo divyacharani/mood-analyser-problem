45b4fe181b4491640f256e39fb7bfac7bebd9cfe
unknown <divyacharaninabha@gmail.com>
2020-10-03 00:47:01 +0530

    Added: Custom Exception added

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.moodanalyser;
 
+import com.bridgelabz.moodanalyser.MoodAnalysisException.EntryType;
+
 public class MoodAnalyser {
 	private String message;
 	
@@ -13,17 +15,18 @@ public class MoodAnalyser {
 	}
 	
 	//Method to analyse mood
-	public String analyseMood() {
+	public String analyseMood() throws MoodAnalysisException {
 		try {
-		if(message.toLowerCase().contains("sad"))
-			return "SAD";
-		else
-			return "HAPPY";
+			if(message.length()==0)
+				throw new MoodAnalysisException(EntryType.EMPTY_ENTRY,"Enter proper Mood!!");
+			if(message.toLowerCase().contains("sad"))
+				return "SAD";
+			else
+				return "HAPPY";
 		}
 		catch(NullPointerException e) {
-			return "HAPPY";
+			throw new MoodAnalysisException(EntryType.NULL_ENTRY,"Enter proper Mood!!");
 		}
 		
 	}
-
 }
