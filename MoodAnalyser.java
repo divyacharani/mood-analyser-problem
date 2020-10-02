a9cde9cc94e509b6c127ddb590006cfcf508fc2f
unknown <divyacharaninabha@gmail.com>
2020-10-02 21:19:45 +0530

    Refractored: Refractored using constructors

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,8 +1,19 @@
 package com.bridgelabz.moodanalyser;
 
 public class MoodAnalyser {
+	private String message;
 	
-	public String analyseMood(String message) {
+	//Constructors
+	public MoodAnalyser() {
+		
+	}
+
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+	
+	//Method to analyse mood
+	public String analyseMood() {
 		if(message.toLowerCase().contains("sad"))
 			return "SAD";
 		else
